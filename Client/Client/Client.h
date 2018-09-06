eb525c728de4c1bfa7e82ff65ae5136a9cbdf57a
Nico <rtrozzo@itba.edu.ar>
2018-09-06 18:03:10 -0300

    creo clases server y client del ejemplo

---
+++ Client/Client/Client.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <cstdio>
+#include <boost/asio.hpp>
+#include <boost/chrono.hpp>
+#include <boost/timer/timer.hpp>
+
+#define HELLO_PORT_STR "50013"
+#define SERVER_IP "localhost"
+
+using namespace std;
+
+class client
+{
+public:
+	client();
+	void startConnection(const char* host);
+	void receiveMessage();
+	~client();
+
+private:
+	boost::asio::io_service* IO_handler;
+	boost::asio::ip::tcp::socket* socket_forClient;
+	boost::asio::ip::tcp::resolver* client_resolver;
+	boost::asio::ip::tcp::resolver::iterator endpoint;
+};
+
